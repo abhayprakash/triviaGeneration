803ba3626a816b9a8aac5333b5af877af9710657
Abhay Prakash <abhay.prakash1@gmail.com>
2014-12-31 22:00:56 +0530

    updated classifier taking movie names as column + some utility changes

--- RCode/classifier_movieName_column.R
+++ RCode/classifier_movieName_column.R
@@ -7,11 +7,11 @@ name <- data[,1]
 training_data <- data[2]
 training_codes <- data[3]
 
-matrix <- create_matrix(training_data, language = "english", removeNumbers=FALSE, stemWords=TRUE, removePunctuation=TRUE, removeStopwords = TRUE, weighting=weightTfIdf, toLower = TRUE)
+matrix <- create_matrix(training_data, language = "english", removeNumbers=FALSE, stemWords=TRUE, removePunctuation=TRUE, removeStopwords = TRUE, toLower = TRUE)
 
-name_id <- as.numeric(data$MOVIE)
-mat <- cbind(name_id, as.matrix(matrix))
+mat_name <- create_matrix(as.numeric(as.factor(name)))
 
+mat <- cbind(mat_name, matrix)
 container <- create_container(mat, t(training_codes), trainSize=1:5904, testSize=5905:7380, virgin=FALSE)
 
 model <- train_model(container, algorithm=c("SVM"), method = "C-classification", cross = 0, cost = 100, kernel = "radial")

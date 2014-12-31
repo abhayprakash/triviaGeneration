436d8f7b3c4cd759b99a747f645f257b37f36a6d
Abhay Prakash <abhay.prakash1@gmail.com>
2014-12-31 21:32:44 +0530

    feature building + selection

---
+++ RCode/Classification_withName.R
@@ -0,0 +1,19 @@
+library(tm)
+library(RTextTools);
+data <- read.csv("trainData_withMovie.txt", sep='\t', header=T)
+data <- data[sample(nrow(data)),]
+
+name <- data[,1]
+training_data <- data[2]
+training_codes <- data[3]
+
+matrix <- create_matrix(training_data, language = "english", removeNumbers=FALSE, stemWords=TRUE, removePunctuation=TRUE, removeStopwords = TRUE, weighting=weightTfIdf, toLower = TRUE)
+
+name_id <- as.numeric(data$MOVIE)
+mat <- cbind(name_id, as.matrix(matrix))
+
+container <- create_container(mat, t(training_codes), trainSize=1:5904, testSize=5905:7380, virgin=FALSE)
+
+model <- train_model(container, algorithm=c("SVM"), method = "C-classification", cross = 0, cost = 100, kernel = "radial")
+results <- classify_model(container, model)
+analytics <- create_analytics(container, results)
--- RCode/naiveBayes.R
+++ RCode/naiveBayes.R
@@ -1,10 +1,58 @@
-library(e1071)
-data <- read.csv("trainData_Merged.txt", sep='\t', header=T)
+# load data
+data <- read.csv("data.txt", sep='\t', header=T)
+
+# see data
+data
+
+# testing syntax for sampling
+sample(5)
+
+# sampling data
 data <- data[sample(nrow(data)),]
-training_data <- data[-2]
-training_codes <- data[2]
-matrix <- create_matrix(training_data, language = "english", removeNumbers=FALSE, stemWords=TRUE, removePunctuation=TRUE, removeStopwords = TRUE, weighting=weightTfIdf)
-mat = as.matrix(matrix)
-classifier = naiveBayes(mat[1:5904, ], as.factor(t(training_codes)[1:5904]))
-predicted = predict(classifier, mat[5905:7380, ])
-table(t(training_codes)[5905:7380], predicted)
+
+# seeing data
+data
+
+# text selection
+data.text <- data[2]
+
+# seeing selected text
+data.text
+
+# label selection
+data.label <- data[3]
+data.product <- data[1]
+
+# taking help
+??create_matrix
+
+# creating matrix with pre-processing
+mat = create_matrix(data.text, language = "english", removeStopwords = TRUE, removePunctuation=TRUE, removeNumbers = FALSE, stemWords = TRUE, tm::weightTfIdf)
+
+# see the mat
+mat
+
+# see the actual matrix format
+as.matrix(mat)
+
+#library used for "naive bayes"
+library(e1071)
+
+# allocating in matrix format
+mat = as.matrix(mat)
+
+mat_pro = create_matrix(data.product, language = "english", removeStopwords = TRUE, removePunctuation=TRUE, removeNumbers = FALSE, stemWords = TRUE, tm::weightTfIdf)
+mat_pro = as.matrix(mat_pro)
+mat = cbind(mat_pro, mat)
+
+# ACTUAL TRAINING THE MODEL with 12 rows
+classifier = naiveBayes(mat[1:12, ], as.factor(data.label[1:12,])) # ..(feature, class_label)
+
+# predicting with 6 rows
+predicted = predict(classifier, mat[13:18, ])
+
+# see the predicted labels
+predicted
+
+#build confusion matrix
+table(data.label[13:18,], predicted)

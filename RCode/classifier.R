f5bb0393a51b423e41ffe758b7424f12b733f209
Abhay Prakash <abhay.prakash1@gmail.com>
2015-01-06 15:00:37 +0530

    + NER featuers, ranked and showed the added features

--- RCode/classifier.R
+++ RCode/classifier.R
@@ -13,19 +13,29 @@ testStart <- trainEnd + 1
 
 matrix <- create_matrix(training_data, language = "english", stripWhitespace = TRUE, removeNumbers=FALSE, stemWords=TRUE, removePunctuation=TRUE, removeStopwords = TRUE, weighting=weightTfIdf)
 
-# taking frequency of superlative POS as feature
-mat <- cbind(as.matrix(matrix), data["superPOS"])
-matrix <- mat
+# + frequency of superlative POS as feature
+matrix <- cbind(as.matrix(matrix), data["superPOS"])
 
+# + frequency of different NERs
+matrix <- cbind(as.matrix(matrix), data[,c("PERSON","ORGANIZATION","DATE","LOCATION","MONEY","TIME")])
 
-# training and testing
+addedFeatures <- c("PERSON","ORGANIZATION","DATE","LOCATION","MONEY","TIME","superPOS")
+
+# converting frequencies to boolean presence
+for(col in addedFeatures)
+{
+  index <- matrix[,col] > 0
+  matrix[index,col] <- 1
+}
+
+############ training and testing
 container <- create_container(matrix, t(training_codes), trainSize=1:trainEnd, testSize=testStart:totalRows, virgin=FALSE)
 model <- train_model(container, algorithm=c("SVM"), method = "C-classification", cross = 0, cost = 100, kernel = "linear")
 results <- classify_model(container, model)
 analytics <- create_analytics(container, results)
 print(analytics@algorithm_summary)
 
-#ranking for features - NOTE: works only for linear kernel
+############# ranking for features - NOTE: works only for linear kernel
 w = t(model$coefs) %*% model$SV
 features <- colnames(matrix)[w@ja]
 weights <- w@ra
@@ -34,11 +44,11 @@ featureWeights$weights <- abs(as.numeric(as.character(featureWeights$weights)))
 featureWeights <- featureWeights[with(featureWeights, order(-weights)),]
 row.names(featureWeights) <- 1:nrow(featureWeights)
 
-# get rank of superlative POS frequency
-cat("Rank of superPOS: ", row.names(featureWeights[featureWeights$features=='superPOS',]), "/", ncol(matrix))
-print(featureWeights[featureWeights$features=='superPOS',])
+# get rank of features
+cat("Total num of NON-ZERO features: ", nrow(featureWeights) , "/" , ncol(matrix))
+print(subset(featureWeights, features %in% addedFeatures))
 
-#predicting
+# predicting
 test_data <- read.csv("interstellar.txt", header=T)
 test_matrix <- create_matrix(test_data, language = "english", removeNumbers=FALSE, stemWords=TRUE, removePunctuation=TRUE, removeStopwords = TRUE, weighting=weightTfIdf)
 test_container <- create_container(test_matrix, rep(1,320), testSize=1:320, virgin=TRUE)

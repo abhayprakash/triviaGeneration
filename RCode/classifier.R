57b83cb52bb85424ccbd0b87bd80b010a1418785
Abhay Prakash <abhay.prakash1@gmail.com>
2015-01-19 23:56:58 +0530

    Did K-folding on all metrices : single function

--- RCode/classifier.R
+++ RCode/classifier.R
@@ -1,11 +1,69 @@
 library(tm)
-library(RTextTools);
+library(RTextTools)
+
+cross_validate_SVM_PRFA <- function(container, nfold, method = "C-classification", cross = 0, cost = 100, kernel = "radial")
+{
+  extract_label_from_prob_names <- function(x) return(rownames(as.matrix(which.max(x))))
+  alldata <- rbind(container@training_matrix, container@classification_matrix)
+  allcodes <- as.factor(c(container@training_codes, container@testing_codes))
+  rand <- sample(nfold, dim(alldata)[1], replace = T)
+  
+  cv_accuracy <- NULL
+  cv_p1 <- NULL
+  cv_r1 <- NULL
+  cv_f1 <- NULL
+  cv_p0 <- NULL
+  cv_r0 <- NULL
+  cv_f0 <- NULL
+  
+  for (i in sort(unique(rand))) {
+    model <- svm(x = alldata[rand != i, ], y = allcodes[rand != i], method = method, cross = cross, cost = cost, kernel = kernel)
+    pred <- predict(model, alldata[rand == i, ])
+    
+    # for comparision: original and predicted
+    true_labels <- as.vector(allcodes[rand == i])
+    predicted_labels <- as.vector(pred, mode = class(true_labels))
+    
+    # accuracy
+    analyze <- predicted_labels == true_labels
+    cv_accuracy[i] <- length(analyze[analyze == TRUE])/length(true_labels)
+    
+    # precision 1 
+    correct_1 <- (predicted_labels == true_labels) & (true_labels == 2)
+    cv_p1[i] <- length(correct_1[correct_1 == TRUE])/length(predicted_labels[predicted_labels == 2])
+    
+    # recall 1
+    cv_r1[i] <- length(correct_1[correct_1 == TRUE])/length(true_labels[true_labels == 2])
+    
+    # f 1
+    cv_f1[i] <- (2*cv_p1[i]*cv_r1[i])/(cv_p1[i]+cv_r1[i])
+    
+    # precision 0
+    correct_0 <- (predicted_labels == true_labels) & (true_labels == 1)
+    cv_p0[i] <- length(correct_0[correct_0 == TRUE])/length(predicted_labels[predicted_labels == 1])
+    
+    # recall 0
+    cv_r0[i] <- length(correct_0[correct_0 == TRUE])/length(true_labels[true_labels == 1])
+    
+    # f 1
+    cv_f0[i] <- (2*cv_p0[i]*cv_r0[i])/(cv_p0[i]+cv_r0[i])
+    
+    cat("Fold ", i, " Out of Sample Accuracy", " = ", cv_accuracy[i], "\n", sep = "")
+    cat("Precision 0: ", cv_p0[i], " Recall 0: ", cv_r0[i]," F 0: ", cv_f0[i], "\n", sep = "")
+    cat("Precision 1: ", cv_p1[i], " Recall 1: ", cv_r1[i]," F 1: ", cv_f1[i], "\n", sep = "")
+  }
+  cat("\nMean Accuracy: ", mean(cv_accuracy), "\n")
+  cat("Mean Precison 0: ", mean(cv_p0), " Mean Recall 0: ", mean(cv_r0), "Mean F 0: ", mean(cv_f0), "\n")
+  cat("Mean Precison 1: ", mean(cv_p1), " Mean Recall 1: ", mean(cv_r1), "Mean F 1: ", mean(cv_f1), "\n")
+}
+
 data <- read.csv("trainData_5K_richFeatures.txt", sep='\t', header=T)
 data <- data[sample(nrow(data)),]
 #load("compareData.RData")
 
 #name <- data[,"MOVIE_NAME_IMDB"]
 training_data <- data["TRIVIA"]
+#data[data$CLASS_V == "Very_Interesting_fact", ]$CLASS_V <- "Interesting_fact"
 training_codes <- data["CLASS"]
 
 totalRows <- nrow(data)
@@ -22,6 +80,8 @@ subject_matrix <- create_matrix(data["SUBJECT_WORDS"], removePunctuation = FALSE
 under_root_matrix <- create_matrix(data["UNDER_ROOT_WORDS"], removePunctuation = FALSE, removeStopwords = FALSE, weighting = weightTf)
 all_linked_entities_matrix <- create_matrix(data["ALL_LINKABLE_ENTITIES_PRESENT"], removePunctuation = FALSE, removeStopwords = FALSE, weighting = weightTf)
 parse_features_matrix <- cbind(as.matrix(all_linked_entities_matrix), as.matrix(root_matrix), as.matrix(subject_matrix), as.matrix(under_root_matrix))
+
+#parse_features_matrix <- cbind(as.matrix(root_matrix), as.matrix(subject_matrix), as.matrix(under_root_matrix))
 
 matrix <- cbind(as.matrix(matrix), as.matrix(parse_features_matrix))
 
@@ -43,14 +103,16 @@ for(col in addedFeatures)
 
 ############ training and testing
 container <- create_container(matrix, t(training_codes), trainSize=1:trainEnd, testSize=testStart:totalRows, virgin=FALSE)
-model <- train_model(container, algorithm=c("SVM"), method = "C-classification", cross = 5, cost = 100, kernel = "linear")
-results <- classify_model(container, model)
-analytics <- create_analytics(container, results)
-print(analytics@algorithm_summary)
+cross_validate_SVM_PRFA(container, 5, "SVM", kernel = "linear")
 
-# cross validate for accuracy
-container <- create_container(matrix, t(training_codes), trainSize=1:totalRows, testSize=NULL, virgin=FALSE)
-print(cross_validate(container, 5, "SVM"))
+  # only on 1 of the folds
+  model <- train_model(container, algorithm=c("SVM"), method = "C-classification", cross = 5, cost = 100, kernel = "linear")
+  results <- classify_model(container, model)
+  analytics <- create_analytics(container, results)
+  print(analytics@algorithm_summary)
+
+  # cross validate for accuracy
+  print(cross_validate(container, 5, "SVM"))
 
 ############# ranking for features - NOTE: works only for linear kernel
 w = t(model$coefs) %*% model$SV

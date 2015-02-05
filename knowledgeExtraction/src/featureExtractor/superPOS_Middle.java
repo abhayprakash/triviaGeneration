d4191c2a3126ad75c060e3fb558fe449332ccde3
Abhay Prakash <abhay.prakash1@gmail.com>
2015-02-05 15:40:39 +0530

    firing superlative POS only when its position is not the start of the sentence

---
+++ knowledgeExtraction/src/featureExtractor/superPOS_Middle.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package featureExtractor;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.IndexedWord;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
+import edu.stanford.nlp.trees.EnglishGrammaticalRelations;
+import edu.stanford.nlp.trees.GrammaticalRelation;
+import edu.stanford.nlp.util.CoreMap;
+import static featureExtractor.NLPFeatures.bw_root;
+import static featureExtractor.NLPFeatures.resultFile_Root;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ *
+ * @author Abhay Prakash
+ */
+public class superPOS_Middle {
+    static StanfordCoreNLP pipeline;
+    static BufferedWriter bw;
+    
+    static String folderPath = "C:\\Users\\Abhay Prakash\\Workspace\\trivia\\Data\\IMDb\\anotherSelected5k\\MORE_DATA\\temp\\";
+    static String resultComp = folderPath + "INT_middleSuperPOS.txt";
+    static String inputFilePath = folderPath + "trivia.txt";
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
+        FileWriter fw = new FileWriter(resultComp);
+        bw = new BufferedWriter(fw);
+        
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos");
+        pipeline = new StanfordCoreNLP(props);
+        
+        FileReader inputFile = new FileReader(inputFilePath);
+        BufferedReader bufferReader = new BufferedReader(inputFile);
+        String input;
+        int lineNum = 0;
+        //input = bufferReader.readLine();
+        while((input = bufferReader.readLine()) != null)
+        {
+            processLine(input, lineNum);
+            System.out.println(lineNum);
+            lineNum++;
+            //break;
+        }
+        bw.close();
+    }
+    
+    static void processLine(String text,int lineId) throws IOException
+    {
+        bw.write(Integer.toString(lineId));
+        int comparativePOS = 0;
+        try{
+            Annotation document = new Annotation(text);
+            pipeline.annotate(document);
+            List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+            for (CoreMap sentence : sentences) {
+                int position = 0;
+                for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                    if(position != 0)
+                    {
+                        position = 1;
+                        continue;
+                    }
+                    String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                    if(pos.equals("JJS") || pos.equals("RBS"))
+                    {
+                        comparativePOS++;
+                    }
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("IGNORED:");
+        }
+        
+        bw.write("\t" + comparativePOS + "\n");
+    }
+}
